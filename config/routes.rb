afb53e846353b30ecf76178fd295d2d1ae34f011
reiji02 <reiji_content@yahoo.co.jp>
2022-03-19 17:13:40 +0000

    create base app

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  
+  root 'index2.html.erb'
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
